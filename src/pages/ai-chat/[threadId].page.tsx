dd259818219f857114e1b5ae396c49cce130bd01
robmolloy <robmolloy@hotmail.co.uk>
2025-06-21 12:53:15 +0100

    small changes

--- src/pages/ai-chat/[threadId].page.tsx
+++ src/pages/ai-chat/[threadId].page.tsx
@@ -1,11 +1,9 @@
 import { AiChatScreen } from "@/modules/aiChat/AiChatScreen";
 import { useRouter } from "next/router";
 
-const aiChatPage = () => {
+export default function () {
   const router = useRouter();
   const threadId = router.query.threadId as string;
 
   return <AiChatScreen threadId={threadId} />;
-};
-
-export default aiChatPage;
+}
