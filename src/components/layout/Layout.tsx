4fb0cb1481a645205e67edbfd8dbc99e6e24102c
robmolloy <robmolloy@hotmail.co.uk>
2025-06-23 23:05:06 +0100

    fix layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -20,15 +20,15 @@ export function Layout(p: { children: React.ReactNode; showLeftSidebar: boolean 
   const currentUserStore = useCurrentUserStore();
 
   return (
-    <div className="relative flex min-h-screen flex-col">
+    <div className="relative flex h-screen flex-col">
       <Header isLoggedIn={currentUserStore.data.status === "loggedIn"} />
       <div className="flex flex-1">
         {p.showLeftSidebar && (
-          <aside className="hidden h-[calc(100vh-3.5rem)] w-64 overflow-y-auto border-r bg-background md:block">
+          <aside className="hidden h-[calc(100vh-58px)] w-64 overflow-y-auto border-r bg-background md:block">
             <LeftSidebar />
           </aside>
         )}
-        <main className="h-[calc(100vh-3.5rem)] w-full overflow-y-auto">{p.children}</main>
+        <main className="h-[calc(100vh-4rem)] w-full overflow-y-auto">{p.children}</main>
       </div>
       <Modal />
     </div>
