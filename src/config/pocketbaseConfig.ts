f17e34e9630bc622202ce6743c897f1f60d00764
robmolloy <robmolloy@hotmail.co.uk>
2025-06-24 18:11:32 +0100

    change pb config

--- src/config/pocketbaseConfig.ts
+++ src/config/pocketbaseConfig.ts
@@ -2,8 +2,8 @@ import PocketBase from "pocketbase";
 
 const pbInstanceMap = {
   pbLocal: () => new PocketBase("http://127.0.0.1:8090"),
-  pbRemote: () => new PocketBase("https://romolo.pockethost.io"),
+  pbRemote: () => new PocketBase("https://pokkit-realtime-chat.pockethost.io"),
 };
 
-export const pb = pbInstanceMap.pbLocal();
+export const pb = pbInstanceMap.pbRemote();
 pb.autoCancellation(false);
