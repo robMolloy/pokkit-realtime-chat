c9b7f354076b7c79e71d391ed336fd27a6a952ee
robmolloy <robmolloy@hotmail.co.uk>
2025-06-28 17:34:09 +0100

    pocketbase config, stop cors

--- src/config/pocketbaseConfig.ts
+++ src/config/pocketbaseConfig.ts
@@ -1,7 +1,7 @@
 import PocketBase from "pocketbase";
 
 const pbInstanceMap = {
-  pbLocal: () => new PocketBase("http://127.0.0.1:8090"),
+  pbLocal: () => new PocketBase("http://0.0.0.0:8090"),
   pbRemote: () => new PocketBase("https://pokkit-realtime-chat.pockethost.io"),
 };
 
